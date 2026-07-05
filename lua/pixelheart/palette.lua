460dc12fbce8452328eb358bb70c10b67a5ed0b2
Marcelo M. Francisco <230030049+aparabatos@users.noreply.github.com>
2026-07-05 17:21:38 -0500

    Add the main palette

---
+++ lua/pixelheart/palette.lua
@@ -0,0 +1,26 @@
+return {
+
+	bg0 = "#1A1A2E",
+	bg1 = "#22223A",
+	bg2 = "#2D2D44",
+	bg3 = "#3A3A5A",
+
+	fg0 = "#FFFFFF",
+	fg1 = "#ECECEC",
+	fg2 = "#C5C5D6",
+	fg3 = "#8A8AA6",
+
+	cyan = "#00F0FF",
+	pink = "#FF0099",
+	purple = "#9D4EDD",
+
+	green = "#00FF88",
+	red = "#FF4D4D",
+
+	yellow = "#FFD166",
+	orange = "#FFB347",
+
+	border = "#4B4B73",
+	selection = "#403A66",
+	cursorline = "#25253B",
+}
